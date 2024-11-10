5ca16340cb000a3fc1f1a6cfe52e4cadf8471945
alvhursa1 <alvaro.hurtado.s@me.com>
2024-11-10 10:31:44 -0500

    v5.2 10-11-24

--- src/components/FterVde.tsx
+++ src/components/FterVde.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link'
 
 export default function Fter() {
   return (
-    <footer className="bg-[#004141] h-[70vh] flex items-center justify-center">
-      <div className="relative w-64 h-64">
-      <Link href="/">
-        <Image
-          src="/images/Logo-rionegro-blanco.svg"
-          alt="Rionegro Logo"
-          layout="fill"
-          objectFit="contain"
-        />
+    <footer className="bg-[#004141] h-[70vh] flex items-center justify-center relative z-[100]">
+      <div className="relative w-40 h-40 sm:w-64 sm:h-64">
+        <Link href="/">
+          <Image
+            src="/images/Logo-rionegro-blanco.svg"
+            alt="Rionegro Logo"
+            layout="fill"
+            objectFit="contain"
+          />
         </Link>
       </div>
     </footer>
